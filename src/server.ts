822533e0dd069a89eb5b3e95e736bb928ee12019
adrianocruz01 <adrianocruz.reis@gmail.com>
2023-11-21 18:23:23 -0300

    feat login endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
-import fastify from 'fastify';
+import fastify, { FastifyBaseLogger, FastifyInstance, FastifyReply, FastifyRequest, FastifySchema, FastifyTypeProviderDefault, RouteGenericInterface } from 'fastify';
 import fastifyMulter from 'fastify-multer';
 import path from 'path';
 import fs from 'fs';
 import cors from '@fastify/cors'
+import pg, { QueryResult } from 'pg';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+const  JWT_SECRET  = process.env.JWT_SECRET as string;
+const { Pool } = pg;
+import  'dotenv/config' ;
+
 
 const app = fastify();
 const upload = fastifyMulter({ dest: './pdfs/' });
@@ -12,6 +19,57 @@ app.register(cors, {
   origin: '*',
   methods: ['POST'],
 })
+
+function generateToken(userId: number) {
+  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: '1h' });
+}
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'authdb',
+  password: '123456789',
+  port: 5432,
+});
+
+pool.connect();
+
+interface User {
+  password_hash(password: string, password_hash: any): unknown;
+  id: number;
+  username: string;
+  password: any;
+}
+
+app.route({
+  method: 'POST',
+  url: '/login',
+  handler: async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const { username, password } = request.body as { username: string, password: string };
+
+      const result: QueryResult<User> = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+   
+      if (result.rows.length === 0) {
+        return reply.code(401).send({ error: 'Credenciais inválidas' });
+      }
+
+      const user = result.rows[0];
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        return reply.code(401).send({ error: 'Credenciais inválidas' });
+      }
+
+      const token = generateToken(user.id);
+
+      reply.send({ token });
+    } catch (error) {
+      console.error('Erro na autenticação:', error);
+      reply.code(500).send('Erro no servidor');
+    }
+  },
+});
 
 app.route({
   method: 'POST',
