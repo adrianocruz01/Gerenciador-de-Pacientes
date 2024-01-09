8afc02f30dc07523f73717a3295b3c8db610d2f6
adriano.cruz01 <adriano.cruz@opportunatecnologia.com.br>
2024-01-09 13:17:20 -0300

    add protect router guard strategy jwt

--- src/libs/patient/patient.controller.ts
+++ src/libs/patient/patient.controller.ts
@@ -1,14 +1,25 @@
-import { Body, Controller, HttpCode, Post, Get, Query, BadRequestException } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Get,
+  Query,
+  BadRequestException,
+  UseGuards,
+} from '@nestjs/common';
 import { CreatePatientService } from './services/create-patient.service';
 import { CreatePatientDto } from './dto/create.patient.dto';
 import { SearchPatientService } from './services/search-patient.service';
 import { SearchPatientDto } from './dto/search.patient.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Public } from '../auth/guards/public';
 
 @Controller('pacientes')
 export class PatientController {
   constructor(
     private readonly createPatientService: CreatePatientService,
-    private readonly searchPatientService: SearchPatientService
+    private readonly searchPatientService: SearchPatientService,
   ) {}
 
   @Post('/cadastrar')
@@ -16,7 +27,9 @@ export class PatientController {
   async register(@Body() createPatientDtos: CreatePatientDto) {
     const patient = await this.createPatientService.execute(createPatientDtos);
     if (!patient) {
-      throw new BadRequestException('Não foi possível criar o paciente com os dados fornecidos.');
+      throw new BadRequestException(
+        'Não foi possível criar o paciente com os dados fornecidos.',
+      );
     }
 
     return {
@@ -29,12 +42,11 @@ export class PatientController {
 
   @Get('/pesquisar')
   @HttpCode(200)
-  async search(@Query() searchPatientDto: SearchPatientDto) { 
+  async search(@Query() searchPatientDto: SearchPatientDto) {
     const patients = await this.searchPatientService.execute(searchPatientDto);
     if (patients.length === 0) {
       return 'Nenhum paciente encontrado.';
     }
     return patients;
   }
-
 }
--- src/libs/patient/patient.module.ts
+++ src/libs/patient/patient.module.ts
@@ -3,10 +3,11 @@ import { PatientController } from './patient.controller';
 import { SearchPatientService } from './services/search-patient.service';
 import { CreatePatientService } from './services/create-patient.service';
 import { PrismaModule } from '../../shared/db/libs/prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-    imports: [PrismaModule],
-    controllers: [PatientController],
-    providers: [SearchPatientService, CreatePatientService]
+  imports: [PrismaModule, AuthModule],
+  controllers: [PatientController],
+  providers: [SearchPatientService, CreatePatientService],
 })
 export class PatientModule {}
