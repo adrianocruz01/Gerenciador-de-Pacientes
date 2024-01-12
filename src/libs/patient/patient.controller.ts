48494b3e9924fbef4b1a15212cff69a00533521b
adriano.cruz01 <adriano.cruz@opportunatecnologia.com.br>
2024-01-12 05:38:49 -0300

    Add new endpoint Post for post the procedure in patient

--- src/libs/patient/patient.controller.ts
+++ src/libs/patient/patient.controller.ts
@@ -9,18 +9,23 @@ import {
   UseGuards,
   Param,
 } from '@nestjs/common';
+import { ParseIntPipe } from '@nestjs/common';
 import { CreatePatientService } from './services/create-patient.service';
 import { CreatePatientDto } from './dto/create.patient.dto';
 import { SearchPatientService } from './services/search-patient.service';
 import { SearchPatientDto } from './dto/search.patient.dto';
 import { GetPatientByIdService } from './services/get-patient-by-id.service';
+import { ProcedimentoService } from './services/register-procedure-by-id.service';
+import { RegisterprocedureDto } from './dto/registerprocedure.patient.dto';
 
 @Controller('pacientes')
 export class PatientController {
+  registerProcedureById: any;
   constructor(
     private readonly createPatientService: CreatePatientService,
     private readonly searchPatientService: SearchPatientService,
-    private readonly getPatientByIdService: GetPatientByIdService
+    private readonly getPatientByIdService: GetPatientByIdService,
+    private readonly procedimentoService: ProcedimentoService
   ) { }
 
   @Post('/cadastrar')
@@ -55,4 +60,19 @@ export class PatientController {
     }
     return patient
   }
+
+  @Post('/:pacienteId/procedimentos')
+  @HttpCode(201)
+  async registrarProcedimento(
+    @Param('pacienteId', ParseIntPipe) pacienteId: number,
+    @Body() registerProcedureDto: RegisterprocedureDto
+  ) {
+    try {
+      const procedimento = await this.procedimentoService.execute(pacienteId, registerProcedureDto);
+      return procedimento;
+    } catch (error) {
+      throw new BadRequestException('Erro ao registrar procedimento: ' + error.message);
+    }
+  }
+
 }
