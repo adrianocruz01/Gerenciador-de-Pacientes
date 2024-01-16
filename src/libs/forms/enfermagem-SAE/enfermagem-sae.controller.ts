1c1aa17b88cc2c91b3a7564ad0bf6092849764ee
adriano.cruz01 <adriano.cruz@opportunatecnologia.com.br>
2024-01-16 17:22:48 -0300

    feat: Create SAE endpoint Serach

--- src/libs/forms/enfermagem-SAE/enfermagem-sae.controller.ts
+++ src/libs/forms/enfermagem-SAE/enfermagem-sae.controller.ts
@@ -2,20 +2,20 @@ import { Controller, Get, HttpCode, Param, Post, Body } from '@nestjs/common';
 import { CreateSAEDto } from './dto/create-sae.dto';
 import { CurrentUser } from 'src/libs/auth/guards/current-user-decorator';
 import { UserPayload } from 'src/libs/auth/jwt-strategy';
-// import { GetSAETriagemService } from './services/get-sae-triagem.service';
 import { CreateSAEFormService } from './service/create-enfermagem-sae.service';
+import { GetEnfermagemSaeFormService } from './service/get-enfermagem-sae.service';
 
 @Controller('fichas/sae-triagem')
 export class SAETriagemController {
   constructor(
     private readonly createSAEFormService: CreateSAEFormService,
-    // private readonly getSAETriagemService: GetSAETriagemService,
+    private readonly getEnfermagemSaeFormService: GetEnfermagemSaeFormService,
   ) { }
 
-  //   @Get(':paciente_procedimento_id')
-  //   async findAll(@Param('paciente_procedimento_id') paciente_procedimento_id: string) {
-  //     return await this.getSAETriagemService.execute(paciente_procedimento_id);
-  //   }
+    @Get(':paciente_procedimento_id')
+    async findAll(@Param('paciente_procedimento_id') paciente_procedimento_id: string) {
+      return await this.getEnfermagemSaeFormService.execute(paciente_procedimento_id);
+    }
 
   @Post()
   @HttpCode(201)
--- src/libs/forms/enfermagem-SAE/enfermagem-sae.module.ts
+++ src/libs/forms/enfermagem-SAE/enfermagem-sae.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/shared/db/libs/prisma/prisma.module';
 import { SAETriagemController } from './enfermagem-sae.controller';
 import { CreateSAEFormService } from './service/create-enfermagem-sae.service';
+import { GetEnfermagemSaeFormService } from './service/get-enfermagem-sae.service';
 
 @Module({
     imports: [PrismaModule],
     controllers: [SAETriagemController],
-    providers: [CreateSAEFormService]
+    providers: [CreateSAEFormService, GetEnfermagemSaeFormService]
 })
 export class EnfermagemSaeModule { }
