d8af4db2e863e2eaad314a4df87ea74d5a8ab402
Yuri <yuriqpaiva@gmail.com>
2024-01-16 12:05:06 -0300

    feat: add get nutritional form endpoint and adaptations

--- src/libs/forms/nutritional/nutritional.controller.ts
+++ src/libs/forms/nutritional/nutritional.controller.ts
@@ -1,26 +1,25 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { NutritionalService } from './services/nutritional.service';
+import { GetNutritionalFormService } from './services/get-nutritional-form.service';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { CreateNutritionalFormService } from './services/create-nutritional-form.service';
 import { CreateNutritionalDto } from './dto/create-nutritional.dto';
 import { CurrentUser } from 'src/libs/auth/guards/current-user-decorator';
 import { UserPayload } from 'src/libs/auth/jwt-strategy';
 
 @Controller('fichas/nutricional')
 export class NutritionalController {
-  constructor(private readonly procedureService: NutritionalService) {}
+  constructor(
+    private readonly CreateNutritionalFormService: CreateNutritionalFormService,
+    private readonly getNutritionalFormService: GetNutritionalFormService,
+  ) {}
+
+  @Get(':paciente_procedimento_id')
+  async findAll(@Param('paciente_procedimento_id') paciente_procedimento_id: string) {
+    return await this.getNutritionalFormService.execute(paciente_procedimento_id);
+  }
 
   @Post()
   @HttpCode(201)
   async register(@CurrentUser() colaborador: UserPayload, @Body() createProcedureDto: CreateNutritionalDto) {
-    return await this.procedureService.create(createProcedureDto, colaborador.sub);
+    return await this.CreateNutritionalFormService.execute(createProcedureDto, colaborador.sub);
   }
-
-  // @HttpCode(200)
-  // @Patch(':id')
-  // async update(
-  //   @Param('patientId') patientId: string,
-  //   @Param('id') id: string,
-  //   @Body() updatePatientProcdeure: UpdateNutritionalDto,
-  // ) {
-  //   return await this.procedureService.update(patientId, id, updatePatientProcdeure);
-  // }
 }
