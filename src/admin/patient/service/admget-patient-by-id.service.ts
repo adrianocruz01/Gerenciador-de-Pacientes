4856dd81188fbffec2999ed27c3a213b4b9f6df2
Yuri <yuriqpaiva@gmail.com>
2024-02-08 21:01:07 -0300

    feat: return qtd_total_fichas on patient by id req.

--- src/admin/patient/service/admget-patient-by-id.service.ts
+++ src/admin/patient/service/admget-patient-by-id.service.ts
@@ -16,6 +16,20 @@ export class AdmGetPatientByIdService {
           Paciente_Procedimento: {
             include: {
               Procedimento: true,
+              _count: {
+                select: {
+                  Ficha_Admissao_Paciente_Unidade: true,
+                  Ficha_Avaliacao_Nutricional: true,
+                  Ficha_Controle_Material: true,
+                  Ficha_Diagnostico_Enfermagem: true,
+                  Ficha_Encaminhamento_Paciente: true,
+                  Ficha_Encaminhamento_Paciente_Cirurgia: true,
+                  Ficha_Intraoperatoria: true,
+                  Ficha_Recebimento_Paciente_Cirurgia: true,
+                  Ficha_SAE_Triagem: true,
+                  Ficha_Transferencia_Paciente: true,
+                },
+              },
             },
           },
         },
@@ -30,6 +44,17 @@ export class AdmGetPatientByIdService {
         procedimentos: patient.Paciente_Procedimento.map((proc) => {
           return {
             id: proc.procedimento_id,
+            qtd_total_fichas:
+              proc._count.Ficha_Admissao_Paciente_Unidade +
+              proc._count.Ficha_Avaliacao_Nutricional +
+              proc._count.Ficha_Controle_Material +
+              proc._count.Ficha_Diagnostico_Enfermagem +
+              proc._count.Ficha_Encaminhamento_Paciente +
+              proc._count.Ficha_Encaminhamento_Paciente_Cirurgia +
+              proc._count.Ficha_Intraoperatoria +
+              proc._count.Ficha_Recebimento_Paciente_Cirurgia +
+              proc._count.Ficha_SAE_Triagem +
+              proc._count.Ficha_Transferencia_Paciente,
             paciente_procedimento_id: proc.paciente_Procedimento_id,
             nome: proc.Procedimento.nome,
             descricao: proc.Procedimento.descricao,
